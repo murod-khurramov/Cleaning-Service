497b552309b117395a5c9513e387ec4dab0adba8
Khurramov <murod_khurramov@icloud.com>
2024-11-23 17:54:46 +0500

    FIX: Reorganize PostController file
    - Removed  due to file misplacement.
    - Re-added  with correct structure and organization.
    - Updated  to ensure compatibility with the updated controller.

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,11 @@ Route::get('/about', [PageController::class, 'about'])->name('about');
 Route::get('/services', [PageController::class, 'services'])->name('services');
 Route::get('/project', [PageController::class, 'project'])->name('project');
 
-Route::get('posts', [PageController::class, 'index']);
+Route::get('posts', [PageController::class, 'index'])->name('posts.index');
+Route::get('posts/{post}', [PageController::class, 'show'])->name('posts.show');
+Route::get('posts/create', [PageController::class, 'create'])->name('posts.create');
+Route::post('posts/create', [PageController::class, 'store'])->name('posts.store');
+Route::get('posts/{post}/edit', [PageController::class, 'edit'])->name('posts.edit');
+Route::put('posts/{post}/edit', [PageController::class, 'update'])->name('posts.update');
+Route::delete('posts/{post}/delete', [PageController::class, 'delete'])->name('posts.delete');
+
