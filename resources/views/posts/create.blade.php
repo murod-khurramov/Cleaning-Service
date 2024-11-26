db129cc160d7b403a61355fc8faf6878a42b8cdb
Khurramov <murod_khurramov@icloud.com>
2024-11-26 14:38:30 +0500

    UI: Update post creation view

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,26 +11,41 @@
         <div class="w-50 py-4">
             <div class="contact-form">
                 <div id="success"></div>
+{{--                @if ($errors->any())--}}
+{{--                    <div class="alert alert-danger">--}}
+{{--                        <ul>--}}
+{{--                            @foreach ($errors->all() as $error)--}}
+{{--                                <li>{{ $error }}</li>--}}
+{{--                            @endforeach--}}
+{{--                        </ul>--}}
+{{--                    </div>--}}
+{{--                @endif--}}
                 <form action="{{ route('posts.store') }}" method="POST">
                     @csrf
-                    <div class="control-group">
-                        <input type="text" class="form-control p-4" name="title" placeholder="Title" required="required" />
-                        <p class="help-block text-danger"></p>
+                    <div class="control-group mb-3">
+                        <input type="text" class="form-control p-4" name="title" value="{{ old('title') }}" placeholder="Title"/>
+                        @error('title')
+                            <p class="help-block text-danger">{{ $message }}</p>
+                        @enderror
                     </div>
-{{--                    <div class="control-group">--}}
-{{--                        <input type="file" class="form-control p-4" id="subject" placeholder="Photo" />--}}
-{{--                        <p class="help-block text-danger"></p>--}}
-{{--                    </div>--}}
+                    {{--                    <div class="control-group">--}}
+                    {{--                        <input type="file" class="form-control p-4" id="subject" placeholder="Photo" />--}}
+                    {{--                        <p class="help-block text-danger"></p>--}}
+                    {{--                    </div>--}}
                     <div class="control-group">
-                            <textarea class="form-control p-4" rows="3" name="short_content" placeholder="Short Content" required="required"></textarea>
-                        <p class="help-block text-danger"></p>
+                        <textarea class="form-control p-4" rows="3" name="short_content" placeholder="Short Content">{{ old('short_content') }}</textarea>
+                        @error('short_content')
+                            <p class="help-block text-danger">{{ $message }}</p>
+                        @enderror
                     </div>
-                    <div class="control-group">
-                            <textarea class="form-control p-4" rows="6" name="content" placeholder="Content" required="required"></textarea>
-                        <p class="help-block text-danger"></p>
+                    <div class="control-group mt-3">
+                        <textarea class="form-control p-4" rows="6" name="content" placeholder="Content">{{ old('content') }}</textarea>
+                        @error('content')
+                            <p class="help-block text-danger">{{ $message }}</p>
+                        @enderror
                     </div>
                     <div>
-                        <button class="btn btn-primary btn-block py-3 px-5" type="submit">
+                        <button class="btn btn-primary btn-block py-3 px-5 mt-3" type="submit">
                             Save
                         </button>
                     </div>
