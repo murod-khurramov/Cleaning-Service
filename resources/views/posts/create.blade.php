28c03cc38858004492d5129693ba83878b1bb414
Khurramov <murod_khurramov@icloud.com>
2024-11-26 19:12:52 +0500

    UI: Update post creation form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,37 +11,41 @@
         <div class="w-50 py-4">
             <div class="contact-form">
                 <div id="success"></div>
-{{--                @if ($errors->any())--}}
-{{--                    <div class="alert alert-danger">--}}
-{{--                        <ul>--}}
-{{--                            @foreach ($errors->all() as $error)--}}
-{{--                                <li>{{ $error }}</li>--}}
-{{--                            @endforeach--}}
-{{--                        </ul>--}}
-{{--                    </div>--}}
-{{--                @endif--}}
-                <form action="{{ route('posts.store') }}" method="POST">
+                {{--                @if ($errors->any())--}}
+                {{--                    <div class="alert alert-danger">--}}
+                {{--                        <ul>--}}
+                {{--                            @foreach ($errors->all() as $error)--}}
+                {{--                                <li>{{ $error }}</li>--}}
+                {{--                            @endforeach--}}
+                {{--                        </ul>--}}
+                {{--                    </div>--}}
+                {{--                @endif--}}
+                <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="control-group mb-3">
-                        <input type="text" class="form-control p-4" name="title" value="{{ old('title') }}" placeholder="Title"/>
+                        <input type="text" class="form-control p-4" name="title" value="{{ old('title') }}"
+                               placeholder="Title"/>
                         @error('title')
                             <p class="help-block text-danger">{{ $message }}</p>
                         @enderror
                     </div>
-                    {{--                    <div class="control-group">--}}
-                    {{--                        <input type="file" class="form-control p-4" id="subject" placeholder="Photo" />--}}
-                    {{--                        <p class="help-block text-danger"></p>--}}
-                    {{--                    </div>--}}
                     <div class="control-group">
-                        <textarea class="form-control p-4" rows="3" name="short_content" placeholder="Short Content">{{ old('short_content') }}</textarea>
-                        @error('short_content')
+                        <input name="photo" type="file" class="form-control p-4" id="subject" placeholder="Photo"/>
+                        @error('photo')
                             <p class="help-block text-danger">{{ $message }}</p>
+                        @enderror                    </div>
+                    <div class="control-group">
+                        <textarea class="form-control p-4" rows="3" name="short_content"
+                                  placeholder="Short Content">{{ old('short_content') }}</textarea>
+                        @error('short_content')
+                        <p class="help-block text-danger">{{ $message }}</p>
                         @enderror
                     </div>
                     <div class="control-group mt-3">
-                        <textarea class="form-control p-4" rows="6" name="content" placeholder="Content">{{ old('content') }}</textarea>
+                        <textarea class="form-control p-4" rows="6" name="content"
+                                  placeholder="Content">{{ old('content') }}</textarea>
                         @error('content')
-                            <p class="help-block text-danger">{{ $message }}</p>
+                        <p class="help-block text-danger">{{ $message }}</p>
                         @enderror
                     </div>
                     <div>
