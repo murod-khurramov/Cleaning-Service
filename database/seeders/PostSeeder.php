7099e939cb64ed81af68163c62c761007881214c
Khurramov <murod_khurramov@icloud.com>
2024-11-28 17:52:48 +0500

    SEED: Add PostSeeder and UserSeeder; update DatabaseSeeder

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        Post::create([
+            'user_id' => 1,
+            'title' => 'Title',
+            'short_content' => 'Short Content',
+            'content' => 'Content',
+            'photo' => null,
+        ]);
+    }
+}
