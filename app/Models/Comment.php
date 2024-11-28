d6f369691eb490f37006d11282824bacf4ebf689
Khurramov <murod_khurramov@icloud.com>
2024-11-28 12:18:26 +0500

    REF: Update Comment model to include additional methods and relationships

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    use HasFactory;
+
+    public function post()
+    {
+        $this->belongsTo(Post::class);
+    }
+
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
 }
