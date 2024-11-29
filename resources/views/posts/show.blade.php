88c1219e5c490afb735efad0835d7b8d0d44e94e
Khurramov <murod_khurramov@icloud.com>
2024-11-29 17:20:30 +0500

    VIEW: Enhance post display layout in show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,7 +13,8 @@
             <div class="row">
                 <div class="col-lg-8">
                     <div class="row mb-4">
-                        <a class="btn btn-sm btn-outline-dark mr-2" href="{{ route('posts.edit', ['post' => $post->id]) }}">
+                        <a class="btn btn-sm btn-outline-dark mr-2"
+                           href="{{ route('posts.edit', ['post' => $post->id]) }}">
                             Edit
                         </a>
                         <form action="{{ route('posts.destroy', ['post' => $post->id]) }}"
@@ -33,46 +34,51 @@
                             <span class="text-primary px-2">|</span>
                             <a class="text-secondary text-uppercase font-weight-medium" href="">Cleaning</a>
                             <span class="text-primary px-2">|</span>
-                            <a class="text-secondary text-uppercase font-weight-medium" href=""> {{ $post->created_at }} </a>
+                            <a class="text-secondary text-uppercase font-weight-medium"
+                               href=""> {{ $post->created_at }} </a>
                         </div>
                         <h1 class="section-title mb-3"> {{ $post->title }} </h1>
                     </div>
 
                     <div class="mb-5">
-                        <img class="img-fluid rounded w-100 mb-4" src="{{ asset('storage/'.$post->photo) }}" alt="Image">
+                        <img class="img-fluid rounded w-100 mb-4" src="{{ asset('storage/'.$post->photo) }}"
+                             alt="Image">
                         <p> {{ $post->content }} </p>
                     </div>
 
                     <div class="mb-5">
                         <h3 class="mb-4 section-title">{{ $post->comments()->count() }} Comments</h3>
-                        <div class="media mb-4">
-                            <img src="/img/user.jpg" alt="Image" class="img-fluid rounded-circle mr-3 mt-1" style="width: 45px;">
-                            <div class="media-body">
-                                <h6>John Doe <small><i>01 Jan 2045 at 12:00pm</i></small></h6>
-                                <p>Diam amet duo labore stet elitr ea clita ipsum, tempor labore accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum clita, at tempor amet ipsum diam tempor sit.</p>
-                                <button class="btn btn-sm btn-light">Reply</button>
+                        @foreach($post->comments as $comment)
+                            <div class="media mb-4">
+                                <img src="/img/user.jpg" alt="Image" class="img-fluid rounded-circle mr-3 mt-1"
+                                     style="width: 45px;">
+                                <div class="media-body">
+                                    <h6> {{ $comment->user->name }} <small><i>{{ $comment->created_at }}</i></small></h6>
+                                    <p>{{ $comment->body }}</p>
+{{--                                    <button class="btn btn-sm btn-light">Reply</button>--}}
+                                </div>
                             </div>
-                        </div>
+                        @endforeach
                     </div>
 
                     <div class="bg-light rounded p-5">
                         <h3 class="mb-4 section-title">Leave a comment</h3>
                         <form action="{{ route('comments.store') }}" method="POST">
                             @csrf
-{{--                            <div class="form-row">--}}
-{{--                                <div class="form-group col-sm-6">--}}
-{{--                                    <label for="name">Name *</label>--}}
-{{--                                    <input type="text" class="form-control" id="name">--}}
-{{--                                </div>--}}
-{{--                                <div class="form-group col-sm-6">--}}
-{{--                                    <label for="email">Email *</label>--}}
-{{--                                    <input type="email" class="form-control" id="email">--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                            <div class="form-group">--}}
-{{--                                <label for="website">Website</label>--}}
-{{--                                <input type="url" class="form-control" id="website">--}}
-{{--                            </div>--}}
+                            {{--                            <div class="form-row">--}}
+                            {{--                                <div class="form-group col-sm-6">--}}
+                            {{--                                    <label for="name">Name *</label>--}}
+                            {{--                                    <input type="text" class="form-control" id="name">--}}
+                            {{--                                </div>--}}
+                            {{--                                <div class="form-group col-sm-6">--}}
+                            {{--                                    <label for="email">Email *</label>--}}
+                            {{--                                    <input type="email" class="form-control" id="email">--}}
+                            {{--                                </div>--}}
+                            {{--                            </div>--}}
+                            {{--                            <div class="form-group">--}}
+                            {{--                                <label for="website">Website</label>--}}
+                            {{--                                <input type="url" class="form-control" id="website">--}}
+                            {{--                            </div>--}}
 
                             <div class="form-group">
                                 <label for="message">Message *</label>
@@ -108,23 +114,28 @@
                         <h3 class="mb-4 section-title">Categories</h3>
                         <ul class="list-inline m-0">
                             <li class="mb-1 py-2 px-3 bg-light d-flex justify-content-between align-items-center">
-                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-secondary mr-2"></i>Web Design</a>
+                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-secondary mr-2"></i>Web
+                                    Design</a>
                                 <span class="badge badge-primary badge-pill">150</span>
                             </li>
                             <li class="mb-1 py-2 px-3 bg-light d-flex justify-content-between align-items-center">
-                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-secondary mr-2"></i>Web Development</a>
+                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-secondary mr-2"></i>Web
+                                    Development</a>
                                 <span class="badge badge-primary badge-pill">131</span>
                             </li>
                             <li class="mb-1 py-2 px-3 bg-light d-flex justify-content-between align-items-center">
-                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-secondary mr-2"></i>Online Marketing</a>
+                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-secondary mr-2"></i>Online
+                                    Marketing</a>
                                 <span class="badge badge-primary badge-pill">78</span>
                             </li>
                             <li class="mb-1 py-2 px-3 bg-light d-flex justify-content-between align-items-center">
-                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-secondary mr-2"></i>Keyword Research</a>
+                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-secondary mr-2"></i>Keyword
+                                    Research</a>
                                 <span class="badge badge-primary badge-pill">56</span>
                             </li>
                             <li class="py-2 px-3 bg-light d-flex justify-content-between align-items-center">
-                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-secondary mr-2"></i>Email Marketing</a>
+                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-secondary mr-2"></i>Email
+                                    Marketing</a>
                                 <span class="badge badge-primary badge-pill">98</span>
                             </li>
                         </ul>
@@ -137,11 +148,13 @@
 
                         @foreach($recent_posts as $post)
                             <div class="d-flex align-items-center border-bottom mb-3 pb-3">
-                                <img class="img-fluid rounded" src="/img/blog-1.jpg" style="width: 80px; height: 80px; object-fit: cover;" alt="">
+                                <img class="img-fluid rounded" src="/img/blog-1.jpg"
+                                     style="width: 80px; height: 80px; object-fit: cover;" alt="">
                                 <div class="d-flex flex-column pl-3">
                                     <a class="text-dark mb-2" href=""> {{ $post->title }} </a>
                                     <div class="d-flex">
-                                        <small><a class="text-secondary text-uppercase font-weight-medium" href="">Admin</a></small>
+                                        <small><a class="text-secondary text-uppercase font-weight-medium"
+                                                  href="">Admin</a></small>
                                         <small class="text-primary px-2">|</small>
                                         <small><a class="text-secondary text-uppercase font-weight-medium" href="">Cleaning</a></small>
                                     </div>
