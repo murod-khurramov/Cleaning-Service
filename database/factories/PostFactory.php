44b3dae7a6cddceb7e6cb88622ab40d8a42a24fa
Khurramov <murod_khurramov@icloud.com>
2024-11-29 18:02:20 +0500

    Database: Update posts migration and factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -18,10 +18,10 @@ class PostFactory extends Factory
     {
         return [
             'user_id' => 1,
+            'category_id' => rand(1, 5),
             'title' => $this->faker->sentence(2),
-            'short_content' => $this->faker->sentence(10),
+            'short_content' => $this->faker->sentence(15),
             'content' => $this->faker->paragraph(15),
-            'photo' => $this->faker->imageUrl(),
         ];
     }
 }
