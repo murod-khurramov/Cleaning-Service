121891a8f3481a75bb185fe25f1884a01ced5061
Khurramov <murod_khurramov@icloud.com>
2024-11-29 15:41:51 +0500

    VIEW: Enhance post display in show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -44,7 +44,7 @@
                     </div>
 
                     <div class="mb-5">
-                        <h3 class="mb-4 section-title">3 Comments</h3>
+                        <h3 class="mb-4 section-title">{{ $post->comments()->count() }} Comments</h3>
                         <div class="media mb-4">
                             <img src="/img/user.jpg" alt="Image" class="img-fluid rounded-circle mr-3 mt-1" style="width: 45px;">
                             <div class="media-body">
@@ -57,26 +57,28 @@
 
                     <div class="bg-light rounded p-5">
                         <h3 class="mb-4 section-title">Leave a comment</h3>
-                        <form>
-                            <div class="form-row">
-                                <div class="form-group col-sm-6">
-                                    <label for="name">Name *</label>
-                                    <input type="text" class="form-control" id="name">
-                                </div>
-                                <div class="form-group col-sm-6">
-                                    <label for="email">Email *</label>
-                                    <input type="email" class="form-control" id="email">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="website">Website</label>
-                                <input type="url" class="form-control" id="website">
-                            </div>
+                        <form action="{{ route('comments.store') }}" method="POST">
+                            @csrf
+{{--                            <div class="form-row">--}}
+{{--                                <div class="form-group col-sm-6">--}}
+{{--                                    <label for="name">Name *</label>--}}
+{{--                                    <input type="text" class="form-control" id="name">--}}
+{{--                                </div>--}}
+{{--                                <div class="form-group col-sm-6">--}}
+{{--                                    <label for="email">Email *</label>--}}
+{{--                                    <input type="email" class="form-control" id="email">--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                            <div class="form-group">--}}
+{{--                                <label for="website">Website</label>--}}
+{{--                                <input type="url" class="form-control" id="website">--}}
+{{--                            </div>--}}
 
                             <div class="form-group">
                                 <label for="message">Message *</label>
-                                <textarea id="message" cols="30" rows="5" class="form-control"></textarea>
+                                <textarea name="body" cols="30" rows="5" class="form-control"></textarea>
                             </div>
+                            <input type="hidden" name="post_id" value="{{ $post->id }}">
                             <div class="form-group mb-0">
                                 <input type="submit" value="Leave Comment" class="btn btn-primary">
                             </div>
