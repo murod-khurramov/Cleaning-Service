41c3877ad732d3ae623b1e52b1e9fb40dbbc2177
Khurramov <murod_khurramov@icloud.com>
2024-11-30 11:52:07 +0500

    Feature: Implement Tag functionality - Created TagController for managing tags. - Added Tag model with relationships. - Created migrations for tags and post_tag pivot table. - Included TagSeeder for initial tag data.

---
+++ database/seeders/TagSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tag;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $tags = [
+            ['name' => 'Design'],
+            ['name' => 'Marketing'],
+            ['name' => 'SEO'],
+            ['name' => 'Writing'],
+            ['name' => 'Consulting'],
+            ['name' => 'Development'],
+            ['name' => 'Reading'],
+        ];
+
+        Tag::insert($tags);
+    }
+}
