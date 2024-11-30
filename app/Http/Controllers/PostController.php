c67360b755bec3f51cc9f78df2d426ac0ea040fa
Khurramov <murod_khurramov@icloud.com>
2024-11-30 13:09:48 +0500

    Post: Improve post listing and details view - Updated PostController for optimized data retrieval in index and show methods. - Enhanced index and show views to display additional post details and styling.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -60,6 +60,8 @@ class PostController extends Controller
         return view('posts.show')->with([
             'post' => $post,
             'recent_posts' => Post::latest()->get()->except($post->id)->take(5),
+            'categories' => Category::all(),
+            'tags' => Tag::all(),
         ]);
     }
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -18,28 +18,30 @@
             </div>
             <div class="row">
 
-            @foreach($posts as $post)
+                @foreach($posts as $post)
 
-                <div class="col-lg-4 col-md-6 mb-5">
-                    <div class="position-relative mb-4">
-                        <img class="img-fluid rounded w-100" src="{{ asset('storage/'.$post->photo) }}" alt="">
-                        <div class="blog-date">
-                            <h4 class="font-weight-bold mb-n1">01</h4>
-                            <small class="text-white text-uppercase">Jan</small>
+                    <div class="col-lg-4 col-md-6 mb-5">
+                        <div class="position-relative mb-4">
+                            <img class="img-fluid rounded w-100" src="{{ asset('storage/'.$post->photo) }}" alt="">
+                            <div class="blog-date">
+                                <h4 class="font-weight-bold mb-n1">01</h4>
+                                <small class="text-white text-uppercase">Jan</small>
+                            </div>
                         </div>
+                        <div class="d-flex mb-2">
+                            @foreach($post->tags as $tag)
+                                <a class="text-secondary text-uppercase font-weight-medium">{{ $tag->name }}</a>
+                                <span class="text-primary px-2">|</span>
+                            @endforeach
+                        </div>
+                        <div class="d-flex mb-2">
+                            <a class="text-danger text-uppercase font-weight-medium">{{ $post->category->name }}</a>
+                        </div>
+                        <h5 class="font-weight-medium mb-2"> {{ $post->title }} </h5>
+                        <p class="mb-4"> {{ $post->short_content }} </p>
+                        <a class="btn btn-sm btn-primary py-2"
+                           href=" {{ route('posts.show', ['post' => $post->id]) }} ">Read More</a>
                     </div>
-                    <div class="d-flex mb-2">
-                        <a class="text-secondary text-uppercase font-weight-medium" href="">Admin</a>
-                        <span class="text-primary px-2">|</span>
-                        <a class="text-secondary text-uppercase font-weight-medium" href="">Cleaning</a>
-                    </div>
-                    <div class="d-flex mb-2">
-                        <a class="text-danger text-uppercase font-weight-medium">{{ $post->category->name }}</a>
-                    </div>
-                    <h5 class="font-weight-medium mb-2"> {{ $post->title }} </h5>
-                    <p class="mb-4"> {{ $post->short_content }} </p>
-                    <a class="btn btn-sm btn-primary py-2" href=" {{ route('posts.show', ['post' => $post->id]) }} ">Read More</a>
-                </div>
                 @endforeach
 
                 {{ $posts->links() }}
