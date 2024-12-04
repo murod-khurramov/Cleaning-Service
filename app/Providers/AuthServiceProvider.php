433ca7044bd4c42e08c442b789372858bce6b75d
Khurramov <murod_khurramov@icloud.com>
2024-12-04 12:33:04 +0500

    Add missing files: Policies and AuthServiceProvider

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        // Model::class => Policy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        Gate::define('update-post', function (User $user, Post $post) {
+            return $user->id === $post->user_id;
+        });
+
+        Gate::define('delete-post', function (User $user, Post $post) {
+            return $user->id === $post->user_id;
+        });
+    }
+}
