ff48c794acbe1051e168e7111fa5b9d0d697e704
Khurramov <murod_khurramov@icloud.com>
2024-12-06 16:22:02 +0500

    Update post-created email view with improved structure and content

--- resources/views/mails/post-created.blade.php
+++ resources/views/mails/post-created.blade.php
@@ -1,1 +1,9 @@
-<?php
+<div>
+    <h1>Dear {{ $post->user->name }}!</h1>
+    <h5>You created a new post at {{ $post->created_at }}</h5>
+    <p>Post ID: {{ $post->id }}</p>
+    <div>{{ $post->title }}</div>
+    <div>{{ $post->short_content }}</div>
+    <div>{{ $post->content  }}</div>
+    <storng>Thank you! </storng>
+</div>
