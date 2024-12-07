cb10ead266ca74dd18da145a3dda6aace60c1b15
Khurramov <murod_khurramov@icloud.com>
2024-12-07 17:46:34 +0500

    Update navbar component: Adjusted layout and styling for better user experience, including improved navigation links and accessibility tweaks.

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,28 +5,35 @@
     <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-        <div class="navbar-nav mr-auto py-0">
-            <a href="/" class="nav-item nav-link active">Home</a>
-            <a href=" {{ route('about') }} " class="nav-item nav-link">About</a>
-            <a href=" {{ route('services') }} " class="nav-item nav-link">Service</a>
-            <a href=" {{ route('project') }} " class="nav-item nav-link">Project</a>
-            <a href=" {{ route('posts.index') }} " class="nav-item nav-link">Blog</a>
-            <a href=" {{ route('contact') }} " class="nav-item nav-link">Contact</a>
-        </div>
-        @auth
-            <div>
-                {{ auth()->user()->name }}
-            </div>
-            <a href="{{ route('posts.create') }}" class="btn btn-primary mr-3 d-none d-lg-block">Create Post</a>
-            <form action="{{ route('logout') }}" method="POST">
-                @csrf
-                <button class="btn btn-danger mr-3 d-none d-lg-block">
-                    Log Out
-                </button>
-            </form>
-        @else
-            <a href="{{ route('login') }}" class="btn btn-primary mr-3 d-none d-lg-block">Login</a>
-        @endauth
+    <div class="navbar-nav mr-auto py-0">
+        <a href="/" class="nav-item nav-link active">Home</a>
+        <a href=" {{ route('about') }} " class="nav-item nav-link">About</a>
+        <a href=" {{ route('services') }} " class="nav-item nav-link">Service</a>
+        <a href=" {{ route('project') }} " class="nav-item nav-link">Project</a>
+        <a href=" {{ route('posts.index') }} " class="nav-item nav-link">Blog</a>
+        <a href=" {{ route('contact') }} " class="nav-item nav-link">Contact</a>
     </div>
+    @auth
+        <a href="" class="btn btn-primary mr-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"/>
+            </svg>
+            <span class="badge badge-light">{{ auth()->user()->notifications()->count() }}</span>
+        </a>
+        <div>
+            {{ auth()->user()->name }}
+        </div>
+        <a href="{{ route('posts.create') }}" class="btn btn-primary mr-3 d-none d-lg-block">Create Post</a>
+        <form action="{{ route('logout') }}" method="POST">
+            @csrf
+            <button class="btn btn-danger mr-3 d-none d-lg-block">
+                Log Out
+            </button>
+        </form>
+    @else
+        <a href="{{ route('login') }}" class="btn btn-primary mr-3 d-none d-lg-block">Login</a>
+        @endauth
+        </div>
 </nav>
