c78b36f7f51af9742d9e3cdde8f33d449d202dcb
Khurramov <murod_khurramov@icloud.com>
2024-12-07 19:41:05 +0500

    Update notifications index view: Improved layout and added support for displaying unread notifications.

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -3,9 +3,9 @@
         Notifications
     </x-slot:title>
 
-{{--    <x-page-header>--}}
-{{--        Notifications--}}
-{{--    </x-page-header>--}}
+    {{--    <x-page-header>--}}
+    {{--        Notifications--}}
+    {{--    </x-page-header>--}}
 
     <!-- Blog Start -->
     <div class="container-fluid py-5">
@@ -15,25 +15,29 @@
                     <h1 class="section-title mb-3">Latest notifications</h1>
                 </div>
             </div>
-                @foreach($notifications as $notification)
+            @foreach($notifications as $notification)
 
-                    <div class="border mb-3 p-4 rounded">
-                        <div class="position-relative mb-4">
+                <div class="border mb-3 p-4 rounded">
+                    <div class="position-relative mb-4">
+                        @if($notification->read_at == null)
                             <div class="blog-date">
                                 <h4 class="font-weight-bold mb-n1">New</h4>
                             </div>
-                        </div>
-                        <div class="d-flex mb-2">
-                            <a class="text-danger text-uppercase font-weight-medium">{{ 'category' }}</a>
-                        </div>
-                        <h5 class="font-weight-medium mb-2"> {{ 'title' }} </h5>
-                        <p class="mb-4"> {{ 'id' }} </p>
-                        <a class="btn btn-sm btn-primary py-2"
-                           href=" {{ 'a' }} ">Reading</a>
+                        @endif
                     </div>
-                @endforeach
+                    <div class="d-flex mb-2">
+                        <a class="text-danger text-uppercase font-weight-medium">{{ $notification->data['created_at'] }}</a>
+                    </div>
+                    <h5 class="font-weight-medium mb-2"> {{ $notification->data['title'] }} </h5>
+                    <p class="mb-4"> {{ 'A new post has been created. ID: '. $notification->data['id'] }} </p>
+                    @if($notification->read_at == null)
+                        <a class="btn btn-sm btn-primary py-2"
+                           href=" {{ route('notifications.read', ['notification' => $notification->id]) }} ">Reading</a>
+                    @endif
+                </div>
+            @endforeach
 
-                {{ $notifications->links() }}
+            {{ $notifications->links() }}
         </div>
     </div>
     <!-- Blog End -->
