16f90a4984bc0361b6c5972c6167cc3500857cf2
Khurramov <murod_khurramov@icloud.com>
2024-12-07 19:40:41 +0500

    Update navbar component: Added dynamic notification

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -14,14 +14,16 @@
         <a href=" {{ route('contact') }} " class="nav-item nav-link">Contact</a>
     </div>
     @auth
-        <a href="{{ route('notifications.index') }}" class="btn btn-primary mr-4">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                 stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                      d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"/>
-            </svg>
-            <span class="badge badge-light">{{ auth()->user()->notifications()->count() }}</span>
-        </a>
+        @if(auth()->user()->unreadNotifications)
+            <a href="{{ route('notifications.index') }}" class="btn btn-primary mr-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                     stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"/>
+                </svg>
+                <span class="badge badge-light">{{ auth()->user()->unreadNotifications()->count() }}</span>
+            </a>
+        @endif
         <div>
             {{ auth()->user()->name }}
         </div>
@@ -34,6 +36,5 @@
         </form>
     @else
         <a href="{{ route('login') }}" class="btn btn-primary mr-3 d-none d-lg-block">Login</a>
-        @endauth
-        </div>
+    @endauth
 </nav>
