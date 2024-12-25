ea122b7104e8dd269cac10553800d80542ac2245
Khurramov <murod_khurramov@icloud.com>
2024-12-25 10:33:45 +0500

    Update console routes

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+})->purpose('Display an inspiring quote')->everyMinute();
+
+Schedule::call(function () {
+    Post::first()->delete();
+})->everyMinute();
