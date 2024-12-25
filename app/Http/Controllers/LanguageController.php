c5f34054a245eb2d8ccffa19fae1182a701b7ade
Khurramov <murod_khurramov@icloud.com>
2024-12-25 12:54:48 +0500

    Add LanguageController for handling localization

---
+++ app/Http/Controllers/LanguageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+class LanguageController extends Controller
+{
+     public function change_locale($locale)
+     {
+          App::setLocale($locale);
+
+          return redirect()->back();
+     }
+}
