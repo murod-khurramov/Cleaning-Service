e1ed92db9b9cb4f28e6060b7cd0f68e439591aa3
Khurramov <murod_khurramov@icloud.com>
2024-12-25 12:53:39 +0500

    Update AppServiceProvider for additional configurations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +23,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrapFour();
+
+        View::composer('components.navbar', function ($view) {
+            $view->with('current_locale', App::currentLocale());
+            $view->with('all_locales', config('app.all_locales'));
+        });
     }
 }
